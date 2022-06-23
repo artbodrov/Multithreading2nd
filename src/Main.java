52707c267cb004eb1300ebbe9a025c1004c21eaf
artbodrov <artem.bodrov@gmail.com>
2022-06-23 18:11:56 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        new Thread(fizzBuzz::fizz).start();
-        new Thread(fizzBuzz::buzz).start();
-        new Thread(fizzBuzz::fizzbuzz).start();
-        new Thread(fizzBuzz::number).start();
-
     }
 }
