c8a34123e413d9642184c5b7c73135f06c300db6
artbodrov <artem.bodrov@gmail.com>
2022-06-24 17:20:59 +0300

    new

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,2 +1,65 @@
+
 public class FizzBuzz {
+    private  int n;
+    private    int  count;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+
+    }
+
+
+    public   void fizz() {
+     for (count = 1; count <= n; ) {
+            if (count % 3 == 0 && count % 5 != 0) {
+                System.out.print("fizz" +count+ ", ");
+               count++;
+
+                Thread.yield();
+            } else {
+                Thread.yield();
+            }
+        }
+  }
+
+
+    public  void buzz() {
+       for (count = 1; count <= n;) {
+            if (count % 5 == 0 && count % 3 != 0) {
+                System.out.print("buzz" +count+ ", ");
+                count++;
+                Thread.yield();
+            } else {
+                Thread.yield();
+            }
+        }
+    }
+
+
+    public   void fizzbuzz() {
+        for (count = 1; count <= n;) {
+            if (count % 3 == 0 && count % 5 == 0) {
+                System.out.print("fizzbuzz" +count+ ", ");
+                count++;
+                Thread.yield();
+
+            } else {
+                Thread.yield();
+            }
+        }
+    }
+
+
+    public  void number() {
+      for (count = 1; count <= n; ) {
+            if (count % 3 != 0 && count % 5 != 0) {
+                System.out.print(count + ", ");
+               count++;
+                Thread.yield();
+
+            } else {
+                Thread.yield();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String args[]) {
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        new Thread(fizzBuzz::fizz).start();
+        new Thread(fizzBuzz::buzz).start();
+        new Thread(fizzBuzz::fizzbuzz).start();
+        new Thread(fizzBuzz::number).start();
     }
 }
